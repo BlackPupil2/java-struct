557e9e0b30cd4aec8b0e00f5a4471e1444d55e2f
BlackPupil2 <1006064830@qq.com>
2021-05-13 09:07:55 +0800

    test5

---
+++ src/lesson3/ArrayList.java
@@ -0,0 +1,44 @@
+package lesson3;
+
+public class ArrayList {
+    //清除元素
+    public void clear(){
+
+    }
+    //元素个数
+    public int size(){
+        return  0;
+    }
+    //是否为空
+    public boolean isEmpty(){
+        return false;
+    }
+    //是否包含某个元素
+    public boolean contains(int element){
+        return  false;
+    }
+    //添加元素到尾部
+    public void add(int elements){
+
+    }
+    //获取index位置的元素
+    public int get(int index){
+        return 0;
+    }
+    //设置index位置的元素
+    public int set(int index,int element){
+        return 0;
+    }
+    //在index位置插入元素
+    public void add(int index,int element){
+
+    }
+    //删除index位置元素
+    public int remove(int index){
+        return 0;
+    }
+    //查看元素索引
+    public int indexOf(int element){
+        return -1;
+    }
+}
---
+++ src/lesson3/Main.java
@@ -0,0 +1,10 @@
+package lesson3;
+
+public class Main {
+    public static void main(String[] args) {
+        int array[]=new int[] {11,22,33};
+        ArrayList list =new ArrayList();
+        list.add();
+        list.remove();
+    }
+}
