a98a11b08f8c1f2f808cf16293b8461d3864c3a7
BlackPupil2 <1006064830@qq.com>
2021-05-15 21:24:43 +0800

    test6

--- src/lesson3/ArrayList.java
+++ src/lesson3/ArrayList.java
@@ -1,44 +1,81 @@
 package lesson3;
 
 public class ArrayList {
+    private int size;
+    private int[] elements;
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final int ELEMENT_NOT_FOUND=-1;
+    public ArrayList(int capacity) {
+        capacity = (capacity < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : capacity;
+        elements = new int[capacity];
+    }
+
+    public ArrayList() {
+        this(DEFAULT_CAPACITY);
+    }
+
     //清除元素
-    public void clear(){
+    public void clear() {
+        size=0;
 
     }
+
     //元素个数
-    public int size(){
-        return  0;
+    public int size() {
+        return size;
     }
+
     //是否为空
-    public boolean isEmpty(){
-        return false;
+    public boolean isEmpty() {
+        return size==0;
     }
+
     //是否包含某个元素
-    public boolean contains(int element){
-        return  false;
+    public boolean contains(int element) {
+        return indexOf(element)!=ELEMENT_NOT_FOUND;
     }
+
     //添加元素到尾部
-    public void add(int elements){
+    public void add(int element) {
+        elements[size]=element;
+        size++;
 
     }
+
     //获取index位置的元素
-    public int get(int index){
-        return 0;
+    public int get(int index) {
+        if(index<0||index>=size){
+            throw new IndexOutOfBoundsException("Index"+index+",Size"+size);
+        }
+        return elements[index];
     }
+
     //设置index位置的元素
-    public int set(int index,int element){
-        return 0;
+    public int set(int index, int element) {
+        if(index<0||index>=size){
+            throw new IndexOutOfBoundsException("Index"+index+",Size"+size);
+        }
+        int old=elements[index];
+        elements[index]=element;
+        return old;
     }
+
     //在index位置插入元素
-    public void add(int index,int element){
+    public void add(int index, int element) {
 
     }
+
     //删除index位置元素
-    public int remove(int index){
+    public int remove(int index) {
         return 0;
     }
+
     //查看元素索引
-    public int indexOf(int element){
-        return -1;
+    public int indexOf(int element) {
+        for (int i = 0; i < size; i++) {
+            if(elements[i]==element){
+                return i;}
+        }
+        return ELEMENT_NOT_FOUND;
     }
 }
--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int array[]=new int[] {11,22,33};
         ArrayList list =new ArrayList();
-        list.add();
-        list.remove();
+       list.get(0);
+       list.set(1)
     }
 }
