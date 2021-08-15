1f991c532127dcffdf4d4f7e1adc18bff0f72e20
BlackPupil2 <1006064830@qq.com>
2021-08-15 10:26:40 +0800

    test8

--- src/lesson3/ArrayList.java
+++ src/lesson3/ArrayList.java
@@ -1,19 +1,21 @@
 package lesson3;
 
-public class ArrayList {
+import java.util.Objects;
+
+public class ArrayList<E> {
     /*
      * 元素数量*/
     private int size;
     /*
      * 所有的元素*/
-    private int[] elements;
+    private E[] elements;
 
     private static final int DEFAULT_CAPACITY = 2;
     private static final int ELEMENT_NOT_FOUND = -1;
 
     public ArrayList(int capacity) {
         capacity = (capacity < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : capacity;
-        elements = new int[capacity];
+        elements = (E[]) new Object[capacity];
     }
 
     public ArrayList() {
@@ -22,8 +24,10 @@ public class ArrayList {
 
     //清除元素
     public void clear() {
-        size = 0;
-
+        for (int i = 0; i < size; i++) {
+            elements[i]=null;
+        }
+    size=0;
     }
 
     //元素个数
@@ -37,32 +41,35 @@ public class ArrayList {
     }
 
     //是否包含某个元素
-    public boolean contains(int element) {
+    public boolean contains(E element) {
         return indexOf(element) != ELEMENT_NOT_FOUND;
     }
 
     //添加元素到尾部
-    public void add(int element) {
+    public void add(E element) {
         add(size,element);
 
     }
 
     //获取index位置的元素
-    public int get(int index) {
+    public E get(int index) {
         rangeCheck(index);
         return elements[index];
     }
 
     //设置index位置的元素
-    public int set(int index, int element) {
+    public E set(int index, E element) {
         rangeCheck(index);
-        int old = elements[index];
+        E old = elements[index];
         elements[index] = element;
         return old;
     }
 
     //在index位置插入元素
-    public void add(int index, int element) {
+    public void add(int index, E element) {
+//        if(element==null) {
+//            return;
+//        }
        rangeCheckforAdd(index);
 
     ensureCapacity(size+1);
@@ -76,23 +83,36 @@ public class ArrayList {
     }
 
     //删除index位置元素
-    public int remove(int index) {
+    public E remove(int index) {
       rangeCheck(index);
-        int old=elements[index];
-        for (int i =index; i < size; i++) {
-            elements[i]=elements[i+1];
+        E old=elements[index];
+        for (int i =index+1; i < size; i++) {
+            elements[i-1]=elements[i];
         }
-        size--;
+
+        elements[--size]=null;
         return old;
     }
-
+    public void remove(E element){
+        remove(indexOf(element));
+    }
     //查看元素索引
-    public int indexOf(int element) {
-        for (int i = 0; i < size; i++) {
-            if (elements[i] == element) {
-                return i;
+    public int indexOf(E element) {
+        if(element == null){
+            for (int i = 0; i < size; i++) {
+                if (elements[i]==null) {
+                    return i;
+                }
+            }
+        }else {
+            for (int i = 0; i < size; i++) {
+                if (element.equals(elements[i])) {
+                    return i;
+                }
             }
         }
+
+
         return ELEMENT_NOT_FOUND;
     }
     /*保证有容量*/
@@ -103,7 +123,7 @@ public class ArrayList {
     }
 //右移1除以2
     int newCapacity=oldCapacity+(oldCapacity>>1);
-    int[] newElements =new int[newCapacity];
+    E[] newElements = (E[]) new Object[newCapacity];
         for (int i = 0; i < size; i++) {
             newElements[i]=elements[i];
         }
--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -2,19 +2,40 @@ package lesson3;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Person> persons =new ArrayList<>();
 
-        ArrayList list =new ArrayList();
-        System.out.println(list.size());
+        persons.add(new Person(11,"gas"));
+        persons.add(new Person(12,"gas2"));
+        persons.add(new Person(13,"gas3"));
+        Assert.test(persons.size() ==4);
+  //  persons.clear();
+    //提醒jvm回收
+    //    System.gc();
+    }
 
 
-        for (int i = 0; i < 20; i++) {
-            list.add(i);
-        }
+    static void test(){
+        //int 包装类 Integer
+        //new向堆空间申请内存
+        //所有类都继承object
+        ArrayList<Person> persons =new ArrayList<>();
+        ArrayList<Integer> list2 =new ArrayList<>();
+        persons.add(new Person(11,"gas"));
+        persons.add(new Person(12,"gas2"));
+        persons.add(new Person(13,"gas3"));
+        System.out.println(persons);
 
-        System.out.println(list);
+//        System.out.println(list.size());
+//
+//
+//        for (int i = 0; i < 20; i++) {
+//            list.add(i);
+//        }
+//
+//        System.out.println(list);
 
-      ;
-    //   System.out.println(list);
+        ;
+        //   System.out.println(list);
 
 
     }
